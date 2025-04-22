ee3f8161aeb9dd7c06facf73e2ab3a7c863d4226
Camila Celente. <camilacelente@yahoo.com.br>
2025-04-22 15:28:25 -0300

    Create  1. CONTATO COM COBOL.cob

---
+++ 1. CONTATO COM COBOL.cob
@@ -0,0 +1,19 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGRAMA02
+      *==============================
+      *======= AUTOR: CAMILA     EMPRESA: XPTO
+      *======= OBJETIVO: 1. CONTATO COM COBOL
+      *======= DATA: 15/02/2025
+      *======= OBSERVAÇÕES:
+      *==============================
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       0002-MAIN-PROCEDURE SECTION.
+            DISPLAY "Hello word"
+       0001-MOVE SECTION
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
