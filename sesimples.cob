4122d56d7592958639e3a8db67b7aff555f54062
Camila Celente. <camilacelente@yahoo.com.br>
2025-08-12 20:15:04 -0300

    Create sesimples.cob

---
+++ sesimples.cob
@@ -0,0 +1,23 @@
+IDENTIFICATION DIVISION.
+       PROGRAM-ID. SESIMPLES.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+           01 WRK-N1      PIC 9(02)V99.
+           01 WRK-N2      PIC 9(02)V99.
+           01 WRK-MEDIA   PIC 9(02)V99.
+
+       PROCEDURE DIVISION.
+           DISPLAY "NOTA 1 "
+              ACCEPT WRK-N1
+           DISPLAY "NOTA 2 "
+              ACCEPT WRK-N2
+
+           COMPUTE WRK-MEDIA = (WRK-N1 + WRK-N2) / 2.
+
+           DISPLAY WRK-MEDIA
+
+           IF WRK-MEDIA GREATER THAN OR EQUAL 7
+               DISPLAY "APROVADO"
+           END-IF.
+
+            STOP RUN.
